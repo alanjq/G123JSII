3254e34a4ac5d76d4fe7b887c43c6f1cc013e989
Alan Jimenez <alanjq@outlook.com>
2023-11-14 21:20:36 -0500

    Reiniciar proyecto

--- semana-1/base-proyecto/src/js/controller.js
+++ semana-1/base-proyecto/src/js/controller.js
@@ -1,7 +1,3 @@
-import { ingredientList } from "./ingredientList.js";
-import { recipeDetailsMarkup } from "./recipe.js";
-import { renderSpinner } from "./spinner.js";
-
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -15,34 +11,3 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
-async function showRecipe(id) {
-  const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
-
-  const { data } = await response.json()
-
-  const { recipe } = data
-
-  return (recipe)
-}
-
-renderSpinner(recipeContainer)
-
-const loadRecipe = () => {
-  const id = location.hash.slice(1);
-  showRecipe(id)
-    .then((recipe) => {
-      // Limpia el contenedor
-      recipeContainer.innerHTML = ''
-      // Colocamos el HTML
-      recipeContainer.insertAdjacentHTML('afterbegin', recipeDetailsMarkup(recipe))
-      recipeContainer.insertAdjacentHTML('afterbegin', ingredientList(recipe))
-    })
-    .catch((er) => {
-      console.error('URL mal', er)
-    })
-}
-
-window.addEventListener('hashchange', loadRecipe)
-
-
