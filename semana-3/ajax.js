90797d8d634fa4bd1b1f7a4ece146a4481705555
Alan Jimenez <alanjq@outlook.com>
2023-11-21 20:58:06 -0500

    Ejemplo con imagen

--- semana-3/ajax.js
+++ semana-3/ajax.js
@@ -1,6 +1,6 @@
 function CargaDocumento() {
     var peticion = new XMLHttpRequest();
-    const URL = 'api/productos.txt'
+    const URL = 'api/imagenes.json'
 
     //  READY STATE
     /* 0 no se ha enviado
@@ -12,8 +12,16 @@ function CargaDocumento() {
         const isCompletado = this.readyState === 4
         const isOk = this.status == 200
         if(isCompletado && isOk){
+            // Texto plano
             // document.getElementById('demostracion').innerHTML = this.responseText
+
+            // Imagen por URL
             document.getElementById('demostracion').setAttribute('src', this.responseText)
+            
+            // JSON
+            const json = JSON.parse( this.response)
+            document.getElementById('demostracion').setAttribute('src', json.imagen)
+            document.getElementById('encabezado').innerHTML = json.titulo
         }
     }
 
